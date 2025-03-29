0fe027a5cc68fca3400b0e0cb49c6fe4ea65bcfa
Ilia Mistiurin <mistyurin.ilya.im@gmail.com>
2025-03-29 21:50:19 +0300

    [REFACTOR] : change package inclusion for better module importing

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+__all__ = ["api"]
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,7 +1,11 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
 
 from .general_model import StatusModel
 
