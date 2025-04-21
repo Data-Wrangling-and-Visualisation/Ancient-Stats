f134293d505dd94cef1b5e9b053a46eeacb627aa
JIeruOH <136506317+JIeruOH@users.noreply.github.com>
2025-04-21 13:52:02 +0300

    Add files via upload

---
+++ flsk.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from requests import get
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    args = {**request.args}
+    if 'hero_id' in args:
+        return get(f'http://localhost:8080/stats/{args.pop("hero_id")}/', args).json()
+    else:
+        return get('http://localhost:8080/stats/', args).json()
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
